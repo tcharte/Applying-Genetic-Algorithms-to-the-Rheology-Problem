a6179ff08dd1809d88260c0994be4b00148bb7a8
Todd <toddcharter97@gmail.com>
2020-11-15 20:33:58 -0800

    ProjectStart
    - Data entered and saved as .mat
    - Created error functions and tested against textbook examples

---
+++ sumOfAbsoluteError.m
@@ -0,0 +1,17 @@
+function f = sumOfAbsoluteError(x)
+% This function calculates the sum of absolute errors
+
+% Load Given Data
+load('viscosity.mat');
+load('strain_rate.mat');
+
+% Parameter Scale and assignment
+eta0 = 520*x(1);
+lambda = 14*x(2);
+beta = 0.038*x(3);
+eta = viscosity;
+gamma_dot = strain_rate;
+
+
+f = sum(eta0*(1+(lambda^2).*(gamma_dot.^2)).^((beta-1)/2)-eta);
+end
